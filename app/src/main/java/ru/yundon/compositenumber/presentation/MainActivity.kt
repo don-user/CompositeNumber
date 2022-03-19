b19e949d4de20be41ee2bc31295182f3f155caef
Yuriy <don_1303@mail.ru>
2022-03-19 16:17:50 +0300

    add_4_fragments_and_4_layout_xml add GameFragment.kt add GameFinishedFragment.kt add ChooseLevelFragment.kt add WelcomeFragment.kt add fragment_choose_level add fragment_game.xml add fragment_game_finished.xml add fragment_welcome.xml add circle_background.xml add ic_brain.xml, ic_sad.xml, ic_smile.xml add style.xml

--- app/src/main/java/ru/yundon/compositenumber/presentation/MainActivity.kt
+++ app/src/main/java/ru/yundon/compositenumber/presentation/MainActivity.kt
@@ -3,10 +3,14 @@ package ru.yundon.compositenumber.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.yundon.compositenumber.R
+import ru.yundon.compositenumber.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy{ ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/ru/yundon/compositenumber/presentation/fragments/ChooseLevelFragment.kt
@@ -0,0 +1,31 @@
+package ru.yundon.compositenumber.presentation.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.yundon.compositenumber.Constants.EXCEPTION_MESSAGE_BINDING
+import ru.yundon.compositenumber.databinding.FragmentChooseLevelBinding
+
+class ChooseLevelFragment : Fragment() {
+
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = _binding ?: throw  RuntimeException (EXCEPTION_MESSAGE_BINDING)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
+}
