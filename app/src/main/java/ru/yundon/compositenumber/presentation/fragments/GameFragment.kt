f91466cc9cc9e7ff2e1dc44359cd3b4a4b6d9880
don-user <don_1303@mail.ru>
2022-03-25 09:35:45 +0300

    fixing bugs in BindingAdapterViewModel

--- app/src/main/java/ru/yundon/compositenumber/presentation/fragments/GameFragment.kt
+++ app/src/main/java/ru/yundon/compositenumber/presentation/fragments/GameFragment.kt
@@ -1,12 +1,9 @@
 package ru.yundon.compositenumber.presentation.fragments
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -21,7 +18,7 @@ class GameFragment : Fragment() {
 
     private val args by navArgs<GameFragmentArgs>()
 
-    private val viewModel by lazy {
+    private val viewModels by lazy {
         ViewModelProvider(
             this,
             GameViewModelFactory(args.level, requireActivity().application)
@@ -43,7 +40,7 @@ class GameFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.viewModel = viewModel
+        binding.viewModel = viewModels
         binding.lifecycleOwner = viewLifecycleOwner
         observeViewModel()
     }
@@ -55,7 +52,7 @@ class GameFragment : Fragment() {
 
     private fun observeViewModel() = with(binding) {
 
-        viewModel.gameResult.observe(viewLifecycleOwner) {
+        viewModels.gameResult.observe(viewLifecycleOwner) {
             launchGameFinishedFragment(it)
         }
     }
